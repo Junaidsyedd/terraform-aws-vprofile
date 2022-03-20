6bf7ab5974b18f1261e49543793080fbcb0750fb
Junaid <junaid2live2@gmail.com>
2022-03-20 01:38:22 -0400

    vpc module

--- vars.tf
+++ vars.tf
@@ -1,98 +1,98 @@
-variable AWS_REGION {
-    default = "us-east-1"
+variable "AWS_REGION" {
+  default = "us-east-1"
 }
 
-variable AMIS {
-    type = map
-    default = {
-        us-east-1 = "ami-04505e74c0741db8d"
-        us-east-2 = "ami-0fb653ca2d3203ac1"
-        ap-south-1 = "ami-0851b76e8b1bce90b"
-    }
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-1  = "ami-04505e74c0741db8d"
+    us-east-2  = "ami-0fb653ca2d3203ac1"
+    ap-south-1 = "ami-0851b76e8b1bce90b"
+  }
 }
 
-variable PRIV_KEY_PATH {
-    default = "vprofilekey"
+variable "PRIV_KEY_PATH" {
+  default = "vprofilekey"
 }
 
-variable PUB_KEY_PATH {
-    default = "vprofilekey.pub"
+variable "PUB_KEY_PATH" {
+  default = "vprofilekey.pub"
 }
 
-variable USERNAME {
-    default = "ubuntu"
+variable "USERNAME" {
+  default = "ubuntu"
 }
 
-variable MYIP {
-    default = "207.38.131.22/32"
+variable "MYIP" {
+  default = "207.38.131.22/32"
 }
 
-variable rmquser {
-    default = "rabbit"
+variable "rmquser" {
+  default = "rabbit"
 }
 
-variable rmqpass {
-    default = "Gr33n@pple123456"
+variable "rmqpass" {
+  default = "Gr33n@pple123456"
 }
 
-variable dbuser {
-    default = "admin"
-}
-
-
-variable dbpass {
-    default = "admin123"
-}
-
-variable dbname {
-    default = "accounts"
-}
-
-variable instance_count {
-    default = "1"
-}
-
-variable VPC_NAME {
-    default = "vprofile-VPC"
+variable "dbuser" {
+  default = "admin"
 }
 
 
-variable Zone1 {
-    default = "us-east-1a"
+variable "dbpass" {
+  default = "admin123"
 }
 
-variable Zone2 {
-    default = "us-east-1b"
+variable "dbname" {
+  default = "accounts"
 }
 
-variable Zone3 {
-    default = "us-east-1c"
+variable "instance_count" {
+  default = "1"
 }
 
-variable VpcCIDR {
-    default = "172.21.0.0/16"
-}
-
-variable PubSub1CIDR {
-    default = "172.21.1.0/24"
-}
-
-variable PubSub2CIDR {
-    default = "172.21.2.0/24"
-}
-
-variable PubSub3CIDR {
-    default = "172.21.3.0/24"
-}
-
-variable PrivSub1CIDR {
-    default = "172.21.4.0/24"
+variable "VPC_NAME" {
+  default = "vprofile-VPC"
 }
 
 
-variable PrivSub2CIDR {
-    default = "172.21.5.0/24"
+variable "Zone1" {
+  default = "us-east-1a"
 }
-variable PrivSub3CIDR {
-    default = "172.21.6.0/24"
+
+variable "Zone2" {
+  default = "us-east-1b"
+}
+
+variable "Zone3" {
+  default = "us-east-1c"
+}
+
+variable "VpcCIDR" {
+  default = "172.21.0.0/16"
+}
+
+variable "PubSub1CIDR" {
+  default = "172.21.1.0/24"
+}
+
+variable "PubSub2CIDR" {
+  default = "172.21.2.0/24"
+}
+
+variable "PubSub3CIDR" {
+  default = "172.21.3.0/24"
+}
+
+variable "PrivSub1CIDR" {
+  default = "172.21.4.0/24"
+}
+
+
+variable "PrivSub2CIDR" {
+  default = "172.21.5.0/24"
+}
+variable "PrivSub3CIDR" {
+  default = "172.21.6.0/24"
 }
